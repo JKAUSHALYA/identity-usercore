11d06d24f4f29806fb74de84f3247abb5903f19e
Damith Senanayake <dracusds123@gmail.com>
2016-02-02 15:57:29 +0530

    Initial commit

--- realm/src/main/java/manager/AuthorizationManager.java
+++ realm/src/main/java/manager/AuthorizationManager.java
@@ -1,10 +1,22 @@
 package manager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by damith on 2/2/16.
  */
 public class AuthorizationManager extends PersistenceManager {
 
+    Map<String, String[]> permissionsOfRoles;
 
+    public AuthorizationManager() {
+        permissionsOfRoles = new HashMap<String, String[]>();
+        permissionsOfRoles.put("ADMIN", new String [] {"/permissions/login"});
+    }
+
+    public boolean isUserAuthorized(String user, String permission) {
+        return false; // implement code here
+    }
 
 }
